c431bb6d3080bad67e801b5fa29304937a376011
Fei <yoooman321@gmail.com>
2018-10-23 00:18:50 +0800

    change image

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,10 +2,13 @@
   <q-page class="flex flex-center">
     <div class="column">
       <div class="img-container row justify-center">
-        <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">              
+        <img alt="Quasar logo" src="~assets/EGG.png">              
       </div>
       <div style="margin: 30px auto;">
-        Hi sweetheart, please change the img above to something else :)
+        Hi sweetheart, please change the img above something else :)
+      </div>
+      <div>
+        Hello World!
       </div>
     </div>
   </q-page>
